01d7c1bcf43f5910f54e5377259c67ea5ef50657
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-01-31 11:43:17 +0900

    model作成

---
+++ app/Models/uesr.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class uesr extends Model
+{
+    use HasFactory;
+}
